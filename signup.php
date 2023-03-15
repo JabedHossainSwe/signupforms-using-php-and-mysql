2e47b81e92ec5a66bea3cd621c535954840cb6df
Jabed Hasan <60073739+JabedHossainJabuu@users.noreply.github.com>
2023-03-16 01:18:58 +0600

    second

--- signup.php
+++ signup.php
@@ -1,33 +1,37 @@
-<?php 
+<?php
 $success = 0;
 $user = 0;
+$invalid = 0;
 
 include 'connect.php';
 
-if($_SERVER['REQUEST_METHOD']=='POST'){
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    
-    $sql = "SELECT * FROM `registration` WHERE username='$username'";
-    $result = mysqli_query($conn,$sql);
-    
-    if ($result) {
-      $num = mysqli_num_rows($result);
-      if($num > 0){
-        $user=1;
-      }
-      else{
-             $sql = "INSERT INTO `registration` (`username`, `password`) VALUES ('$username', '$password')";
-             $result = mysqli_query($conn,$sql);
-        
-             if($result){
-               $success=1;
-             }else{
-               die(mysqli_error($conn));
-              }
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  $confirm_password = $_POST['confirm_password'];
+
+  $sql = "SELECT * FROM `registration` WHERE username='$username'";
+  $result = mysqli_query($conn, $sql);
+
+  if ($result) {
+    $num = mysqli_num_rows($result);
+    if ($num > 0) {
+      $user = 1;
+    } else {
+      if ($password === $confirm_password) {
+        $sql = "INSERT INTO `registration` (`username`, `password`) VALUES ('$username', '$password')";
+        $result = mysqli_query($conn, $sql);
+
+        if ($result) {
+          $success = 1;
+          header('location:login.php');
+        }
+      } else {
+        $invalid = 1;
       }
     }
   }
+}
 
 
 ?>
@@ -35,55 +39,75 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
 
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Signup Page</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-  </head>
+  <title>Signup Page</title>
 
-  <body>
+</head>
 
-  <?php 
+<body>
 
-    if ($user) {
-      echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+  <?php
+  if ($user) {
+    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
       <strong>sorry!</strong> User already exits.
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>';
-    }
+  }
   ?>
 
-  <?php 
+  <?php
+  if ($invalid) {
+    echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+      <strong>sorry!</strong> Password did not match.
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>';
+  }
+  ?>
 
-    if ($success) {
-      echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <?php
+
+  if ($success) {
+    echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
       <strong>Success!</strong> You are successfully signed up.
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>';
-    }
+  }
   ?>
 
 
-    <h1 class="text-center"> Signup Page</h1>
-    <div class="container mt-5">
+  <h1 class="text-center"> Signup Page</h1>
+  <div class="container mt-5">
     <form action="signup.php" method="post">
-  <div class="mb-3">
-    <label for="username" class="form-label">Username: </label>
-    <input type="text" name="username" class="form-control" placeholder="Enter you Username">
-  </div>
-  <div class="mb-3">
-    <label for="password" class="form-label">Password</label>
-    <input type="password" name="password" class="form-control" placeholder='Enter your Password'>
-  </div>
-  <button type="submit" class="btn btn-primary w-100">Sign up</button>
-</form>
-    </div>
+      <div class="mb-3">
+        <label for="username" class="form-label">Username: </label>
+        <input type="text" name="username" class="form-control" placeholder="Enter you Username">
+      </div>
+      <div class="mb-3">
+        <label for="password" class="form-label">Password</label>
+        <input type="password" name="password" class="form-control" placeholder='Enter your Password'>
+      </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+      <div class="mb-3">
+        <label for="confirm_password" class="form-label">Confirm Password</label>
+        <input type="password" name="confirm_password" class="form-control" placeholder='Confirm Password'>
+      </div>
 
-  </body>
+      <button type="submit" class="btn btn-primary w-100">Sign up</button>
+
+    </form>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+    crossorigin="anonymous"></script>
+
+</body>
+
 </html>
